d29fac97fb16f9f4a732203c61399a51f2f78b80
QuantumQuacken <10146689+QuantumQuacken@users.noreply.github.com>
2022-08-20 03:47:18 +0200

    Introduced a way to stop redrawing Vis while it's being changed.

---
+++ ArcenXE/ArcenXE/Utilities/MessagePumpOverriding.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace ArcenXE.Utilities
+{
+    public static class MessagePumpOverriding
+    {
+        [DllImport( "user32.dll" )]
+        public static extern int SendMessage( IntPtr hWnd, Int32 wMsg, bool wParam, Int32 lParam );
+
+        private const int WM_SETREDRAW = 11;
+
+        public static void SuspendDrawing( Control parent )
+        {
+            _ = SendMessage( parent.Handle, WM_SETREDRAW, false, 0 );
+        }
+
+        public static void ResumeDrawing( Control parent )
+        {
+            _ = SendMessage( parent.Handle, WM_SETREDRAW, true, 0 );
+            parent.Refresh();
+        }
+    }
+}
